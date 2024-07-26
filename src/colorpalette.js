995e542cfc70fa805d82e644d6848163ae6ded8c
halfeastx7 <hafizizan@gmail.com>
2024-07-26 10:33:02 +0800

    Upload Api

---
+++ src/colorpalette.js
@@ -0,0 +1,46 @@
+const colorpalette = [
+    {
+        id: 1,
+        color: "Red",
+        hexnumber: "#FF0000"
+    },
+    {
+        id: 2,
+        color: "Blue",
+        hexnumber: "#0000FF"
+    }, {
+        id: 3,
+        color: "White",
+        hexnumber: "#FFFFFF"
+    }, {
+        id: 4,
+        color: "Yellow",
+        hexnumber: "#FFFF00"
+    }, {
+        id: 5,
+        color: "Green",
+        hexnumber: "#00FF00"
+    }, {
+        id: 6,
+        color: "Brown",
+        hexnumber: "#A52A2A"
+    }, {
+        id: 7,
+        color: "Grey",
+        hexnumber: "#808080"
+    }, {
+        id: 8,
+        color: "Purple",
+        hexnumber: "#800080"
+    }, {
+        id: 9,
+        color: "Pink",
+        hexnumber: "#FFC0CB"
+    }, {
+        id: 10,
+        color: "Black",
+        hexnumber: "#000000"
+    },
+]
+export default colorpalette;
+
